b595cf7ab6fd16de989f05360827a9b605a6282d
Sachin <145639140+Derikklok@users.noreply.github.com>
2026-04-09 23:10:26 +0530

    feat: update Angular Material dependencies and implement basic UI components
    - Added Angular CDK and Material dependencies to package.json.
    - Replaced placeholder content in app.html with a simple "Hello World!" message and a button.
    - Integrated Angular Material components (MatButton and MatSlideToggle) in app.ts.
    - Updated index.html to include Google Fonts and Material Icons.
    - Configured global styles in styles.scss to support Angular Material theming.

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggle, MatButton],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
