2826b4672f8c25c7e6553e9d439bd7dcf500f174
Sachin <145639140+Derikklok@users.noreply.github.com>
2026-04-22 23:25:22 +0530

    feat: create Dashboard component and update Home page links

---
+++ client/client-2/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+export default function Dashboard(){
+    return(
+        <div className="m-5">
+            <h1 className="font-bold text-2xl">Dashboard</h1>
+            <p>Content goes here</p>
+
+            <div>
+                something
+            </div>
+        </div>
+    )
+}
--- client/client-2/app/page.tsx
+++ client/client-2/app/page.tsx
@@ -2,13 +2,16 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
+    <div className="m-4">
       <h1>Welcome to Next</h1>
       <Link href="/about" className="text-blue-600 m-5 hover:underline">
         About
       </Link>
-      <Link href="/tasks" className="text-blue-600 hover:underline">
+      <Link href="/tasks" className="text-blue-600 m-5 hover:underline">
         Tasks
+      </Link>
+      <Link href="/dashboard" className="text-blue-600 m-5 hover:underline">
+        Dashboard
       </Link>
     </div>
   );
